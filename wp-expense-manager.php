c0cb210b9a7eeb085199a45b136c91b60c1cd9a9
Mak Alamin <makalamin012@gmail.com>
2021-11-18 22:51:10 +0600

    First Commit

---
+++ wp-expense-manager.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Plugin Name: WP Expense Manager
+ * Plugin URI: https://example.com/plugins/the-basics/
+ * Description: Handle the basics with this plugin.
+ * Version: 1.0.0
+ * Requires at least: 5.2
+ * Requires PHP: 7.2
+ * Author: Mak Alamin
+ * Author URI:
+ * License: GPL v2 or later
+ * License URI: https: //www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:
+ * Text Domain: wp-expense-manager
+ * Domain Path: /languages
+ */
